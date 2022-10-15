733df8b3433990dc1cef8a3e42b04b943e97c35d
Maxim Nikiforov <Rezusjkee@outlook.com>
2022-10-16 00:23:05 +0500

    урок 23 Форматирование строк souf

--- src/Lesson23.java
+++ src/Lesson23.java
@@ -8,11 +8,19 @@ public class Lesson23 {
         String stringall = string1 + string2 + string3;
         System.out.println(stringAll);
          */
-StringBuilder sb = new StringBuilder("Hello");
+        StringBuilder sb = new StringBuilder("Hello");
         System.out.println(sb.toString());
         sb.append(" my").append(" friend.");
         System.out.println(sb.toString());
 
+
+        // Форматирование строк souf
+//        System.out.printf("%fThis is %s a string, %d", 10.4, "LOL", 75);
+        System.out.printf("String %10d\n", 523);
+        System.out.printf("String %10d\n", 1000000000);
+        System.out.printf("String %-10d\n", 100000);
+        System.out.printf("String %.2f\n", 52.3);
+        System.out.printf("String %.4f\n", 52.3);
 
 
     }
